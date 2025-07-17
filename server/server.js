86770ba6088d31030cf91da779db6f189b911ac7
Kumaresh200 <skumaresh200@gmail.com>
2025-07-17 11:29:40 +0530

    Connected MongoDB Successfully

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 5000;
+
+// MongoDB Connection
+mongoose.connect('mongodb+srv://admin:<db_password>@fooddeliveryapp.bjpbyzu.mongodb.net/?retryWrites=true&w=majority&appName=FoodDeliveryApp', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB Connected Successfully!'))
+.catch((err) => console.log('MongoDB Connection Failed:', err));
+
+app.use(express.json());
+
+// Sample Route
+app.get('/', (req, res) => {
+  res.send('Backend Working Fine with MongoDB!');
+});
+
+app.listen(port, () => {
+  console.log('Server running on port ${port}');
+});
