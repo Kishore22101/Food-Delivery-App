de63e069f46a5287eaf0661ec341619abc6f6b1e
kaviyan1409 <kaviyan5242@gmail.com>
2025-07-17 14:09:54 +0530

    Added Navbar and Home components with folder structure

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar() {
+return (
+<nav>
+<h1>Food Delivery App</h1>
+<ul>
+<li>Home</li>
+<li>Menu</li>
+<li>Contact</li>
+</ul>
+</nav>
+);
+}
+export default Navbar;
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+return (
+<div>
+<h2>Welcome to Our Food Delivery App!</h2>
+<p>Order your favorite meals now.</p>
+</div>
+);
+}
+export default Home;
