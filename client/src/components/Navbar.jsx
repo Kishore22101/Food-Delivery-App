245c7b03da6968225b1937faddbcb18699fbcad4
Kishore22101 <kishorek1722@gmail.com>
2025-07-21 22:25:54 +0530

    small changes in Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,17 @@
+import { Link } from 'react-router-dom';
+
 function Navbar() {
-return (
-<nav>
-<h1>Food Delivery App</h1>
-<ul>
-<li>Home</li>
-<li>Menu</li>
-<li>Contact</li>
-</ul>
-</nav>
-);
+  return (
+    <nav>
+      <h1>Food Delivery App</h1>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/menu">Menu</Link></li>
+        <li><Link to="/cart">Cart</Link></li>
+        <li><Link to="/login">Login</Link></li>
+      </ul>
+    </nav>
+  );
 }
+
 export default Navbar;
