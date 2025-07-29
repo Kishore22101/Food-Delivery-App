45bb126c17c63639e0c239d64dd2758d7596a20f
Kishore22101 <kishorek1722@gmail.com>
2025-07-29 20:59:27 +0530

    Initial commit - EatzUp May Online Project

--- server/middleware/errorHandler.js
+++
@@ -1,6 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-  console.error('🔥 Error:', err.message);
-  res.status(500).json({ message: 'Something went wrong', error: err.message });
-};
-
-export default errorHandler;
